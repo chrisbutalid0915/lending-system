cf93dc3958c7dbf67bf48e9c065407c9b8310f02
Kuriz15 <chrisbutalid@gmail.com>
2023-09-11 17:23:41 +0800

    add login style

--- frontend/src/components/LoginForm.vue
+++ frontend/src/components/LoginForm.vue
@@ -1,16 +1,72 @@
 <template>
-    <div>
-        <h1>Login</h1>
-        <form @submit.prevent="authenticate">
-            <label for="username">Username:</label>
-            <input type="text" v-model="username" id="username" required>
-            <br>
-            <label for="password">Password:</label>
-            <input type="password" v-model="password" id="password" required>
-            <br>
-            <button type="submit">Login</button>
-        </form>
+  <div class="flex min-h-full flex-1">
+    <div class="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+      <div class="mx-auto w-full max-w-sm lg:w-96">
+        <div>
+          <img class="h-10 flex w-full"
+               src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+               alt="" />
+          <h2 class="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div class="mt-10">
+          <div>
+            <form @submit.prevent="authenticate" class="space-y-6">
+              <div>
+                <label for="username" class="block text-sm font-medium leading-6 text-gray-900">
+                  Username
+                </label>
+                <div class="mt-2">
+                  <input id="username"
+                         name="username"
+                         type="text"
+                         autocomplete="email"
+                         required=""
+                         placeholder="username"
+                         v-model="username"
+                         class="bg-none block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                </div>
+              </div>
+
+              <div>
+                <label for="password"
+                       class="block text-sm font-medium leading-6 text-gray-900">
+                  Password
+                </label>
+                <div class="mt-2">
+                  <input id="password"
+                         name="password"
+                         type="password"
+                         autocomplete="current-password"
+                         required=""
+                         placeholder="password"
+                         v-model="password"
+                         class="bg-none block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                </div>
+              </div>
+
+              <div>
+                <button type="submit"
+                        class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                  Sign in
+                </button>
+              </div>
+            </form>
+          </div>
+
+        </div>
+      </div>
     </div>
+
+    <div class="relative hidden w-0 flex-1 lg:block">
+      <img class="absolute inset-0 h-full w-full object-cover"
+           src="https://images.unsplash.com/photo-1496917756835-20cb06e75b4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
+           alt="" />
+    </div>
+
+  </div>
 </template>
 
 <script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import axios from "axios";
 import store from '@/store';
 import router from "@/router";
+import '@/tailwind.css'
 
 axios.defaults.baseURL = "http://127.0.0.1:8000"
 createApp(App).use(store).use(router).mount('#app')
